6fb0b9bde0b6846ed9770657ad950afc9e4efeb0
Johann Eicher <johanneicher@gmail.com>
2016-05-04 16:04:13 +0200

    Fid and FidArray with 100% test coverage

--- nmrpy/data_objects.py
+++ nmrpy/data_objects.py
@@ -2,32 +2,51 @@ import numpy as np
 import scipy as sp
 import pylab as pl
 import lmfit
-
+import sys
 
 class Fid():
     
     def __init__(self, *args, **kwargs):
-        self.__set_id(kwargs.get('id', 0))
+        self.__set_id(kwargs.get('id', 'fid0'))
         self.set_data(kwargs.get('data', []))
+
+    def __str__(self):
+        return 'FID: %s (%i data)'%(self.get_id(), len(self.get_data()))
 
     def get_id(self):
         return self.id
 
     def __set_id(self, id):
-        id_type = type(id)
-        if id_type is int:
+        if isinstance(id, str):
             self.id = id
         else:
-            raise AttributeError('ID must be an integer.')
+            raise AttributeError('id must be a string.')
 
     def get_data(self):
         return self.data
         
     def set_data(self, data):
-        if not isinstance(data, str) and self._is_iter(data) and not any(self._is_iter(i) for i in data):
+        if self._is_valid_dataset(data):
             self.data = list(data)
-        else:
-            raise AttributeError('Data must be a flat iterable of numbers.')
+
+    def _is_valid_dataset(self, data):
+        if isinstance(data, str):
+            raise AttributeError('Data must be iterable not a string.')
+        if not self._is_iter(data):
+            raise AttributeError('Data must be an iterable.')
+        if not Fid._is_flat_iter(data):
+            raise AttributeError('Data must not be nested.')
+        if not all(isinstance(i, (int, float, complex)) for i in data):
+            raise AttributeError('Data must consist of numbers only.')
+        return True 
+        
+
+    @classmethod
+    def from_data(cls, data):
+       new_instance = cls()
+       new_instance.set_data(data)
+       return new_instance  
+        
 
     @staticmethod
     def _is_iter(i):
@@ -37,12 +56,21 @@ class Fid():
         except TypeError:
             return False
 
-    @staticmethod
-    def _is_iter_of_iters(i):
-        if self._is_iter(i) and all(self._is_iter(j) for j in i):
-            return True
-        else:
+    @classmethod
+    def _is_iter_of_iters(cls, i):
+        if i == []:
             return False
+        elif cls._is_iter(i) and all(cls._is_iter(j) for j in i):
+            return True
+        return False
+
+    @classmethod
+    def _is_flat_iter(cls, i):
+        if i == []:
+            return True
+        elif cls._is_iter(i) and not any(cls._is_iter(j) for j in i):
+            return True
+        return False
 
 class FidArray():
 
@@ -58,10 +86,37 @@ class FidArray():
         else:
             raise AttributeError('ID must be a string or None.')
 
+    def add_fid(self, fid):
+        if isinstance(fid, Fid):
+            setattr(self, fid.get_id(), fid)
+        else:
+            raise AttributeError('FidArray requires Fid object.')
+
+    def add_fids(self, fids):
+        if self._is_iter(fids):
+            for fid in fids:
+                try:
+                    self.add_fid(fid)
+                except AttributeError as e:
+                    print(e)
+
+    def get_fid(self, id):
+        try:
+            return getattr(self, id)
+        except AttributeError:
+            print('{} does not exist.'.format(id))
+
     @classmethod
-    def from_iterable(cls, data_array):
-        if not cls._is_iter_of_iters(data_array):
-            raise AttributeError('data_array must be an iterable of iterables.')
+    def from_data(cls, data):
+        if not cls._is_iter_of_iters(data):
+            raise AttributeError('data must be an iterable of iterables.')
+        fid_array = cls()
+        fids = []
+        for fid_index, datum in zip(range(len(data)), data):
+            fid_id = 'fid%i'%fid_index
+            fids.append(Fid(id=fid_id, data=datum))
+        fid_array.add_fids(fids)
+        return fid_array
 
     @staticmethod
     def _is_iter(i):
@@ -71,9 +126,13 @@ class FidArray():
         except TypeError:
             return False
 
-    @staticmethod
-    def _is_iter_of_iters(i):
-        if FidArray._is_iter(i) and all(FidArray._is_iter(j) for j in i):
-            return True
-        else:
+    @classmethod
+    def _is_iter_of_iters(cls, i):
+        if i == []:
             return False
+        elif cls._is_iter(i) and all(cls._is_iter(j) for j in i):
+            return True
+        return False
+
+if __name__ == '__main__':
+    pass
--- tests/nmrpy_tests.py
+++ tests/nmrpy_tests.py
@@ -1,32 +1,70 @@
 import unittest
 from nmrpy.data_objects import FidArray, Fid
-
+import numpy
 
 class TestFidInitialisation(unittest.TestCase):
     
     def setUp(self):
-        self.fid_data = [1, 2.0, 3.0+1j] 
+        self.fid_good_data = [[],
+                            [1, 2.0, 3.0+1j],
+                            numpy.array([1, 2.0, 3.0+1j])
+                            ]
+        self.fid_bad_data = [
+                        'string',
+                        1,
+                        [1, [2]],
+                        [1, 2.0, 'string'],
+                        [1, 2.0, Fid()],
+                        ]
+
+    def test_str(self):
+        fid = Fid()
+        self.assertIsInstance(fid.__str__(), str)
 
     def test_is_iter(self):
-        self.assertTrue(Fid._is_iter(self.fid_data))
+        for data in self.fid_good_data:
+            self.assertTrue(Fid._is_iter(data))
         self.assertFalse(Fid._is_iter(1))
 
     def test_fid_assignment(self):
         fid = Fid()
-        self.assertIsInstance(fid.get_id(), int)
+        self.assertIsInstance(fid.get_id(), str)
         self.assertIsInstance(fid.get_data(), list)
         self.assertFalse(any(self._is_iter(i) for i in fid.get_data()))
-        fid = Fid(id=1, data=self.fid_data)
-        self.assertIsInstance(fid.get_id(), int)
+        fid = Fid(id='string', data=self.fid_good_data[0])
+        self.assertIsInstance(fid.get_id(), str)
         self.assertIsInstance(fid.get_data(), list)
         self.assertFalse(any(self._is_iter(i) for i in fid.get_data()))
 
     def test_failed_fid_assignment(self):
-        with self.assertRaises(AttributeError):
-            Fid(id='string')
-        with self.assertRaises(AttributeError):
-            Fid(data='string')
+        for test_id in [1, []]:
+            with self.assertRaises(AttributeError):
+               Fid(id=test_id)
+        for test_data in self.fid_bad_data:
+            with self.assertRaises(AttributeError):
+               Fid(data=test_data)
     
+    def test_fid_from_data(self):
+        for data in self.fid_good_data:
+            fid = Fid.from_data(data)
+            self.assertEqual(fid.get_data(), list(data))
+        
+    def test_fid_from_data_failed(self):
+        for test_data in self.fid_bad_data:
+            with self.assertRaises(AttributeError):
+               Fid.from_data(test_data)
+
+    def test__is_iter_of_iters(self):
+        Fid._is_iter_of_iters([[]])
+
+    def test_failed__is_iter_of_iters(self):
+        for i in [
+                [],
+                [1, 3],
+                [1, [2]],
+                ]:
+            self.assertFalse(Fid._is_iter_of_iters(i))
+
     @staticmethod
     def _is_iter(i):
         try:
@@ -45,8 +83,10 @@ class TestFidInitialisation(unittest.TestCase):
 
 class TestFidArrayInitialisation(unittest.TestCase):
     
-    #def setUp(self):
-    #    self.fid_data = [1, 2.0, 3.0+1j] 
+    def setUp(self):
+        self.fid_data = [1, 2.0, 3.0+1j]
+        self.fid = Fid(id='fid0', data=self.fid_data)
+        self.fids = [Fid(id='fid%i'%id, data=self.fid_data) for id in range(10)]
 
     def test_fid_array_assignment(self):
         fid_array = FidArray()
@@ -58,11 +98,51 @@ class TestFidArrayInitialisation(unittest.TestCase):
         with self.assertRaises(AttributeError):
             FidArray(id=1)
     
-    def test_failed_fid_array_from_iterable(self):
+    def test_failed_fid_array_from_dataable(self):
         fid_data_array = [1, 2.0, 3.0+1j] 
         with self.assertRaises(AttributeError):
-            FidArray.from_iterable(fid_data_array)
+            FidArray.from_data(fid_data_array)
 
+    def test_fid_array_add_fid(self):
+        fid_array = FidArray()
+        fid_array.add_fid(self.fid)
+        self.assertEqual(fid_array.get_fid(self.fid.get_id()), self.fid)
+
+    def test_failed_fid_array_get_fid(self):
+        fid_array = FidArray()
+        self.assertEqual(fid_array.get_fid('non_existent_fid'), None)
+
+    def test_failed_fid_array_add_fid(self):
+        fid_array = FidArray()
+        with self.assertRaises(AttributeError):
+            fid_array.add_fid('string')
+
+    def test_fid_array_add_fid(self):
+        fid_array = FidArray()
+        fid_array.add_fids(self.fids)
+
+    def test_failed_fid_array_add_fid(self):
+        fid_array = FidArray()
+        fid_array.add_fids(self.fids+['string'])
+
+    def test_from_data(self):
+        data_array = 3*[self.fid_data] 
+        fid_array = FidArray.from_data(data_array)
+        self.assertIsInstance(fid_array, FidArray)
+        for fid_id in ['fid%i'%i for i in range(len(data_array))]:
+            fid = fid_array.get_fid(fid_id)
+            self.assertIsInstance(fid, Fid)
+
+    def test__is_iter_of_iters(self):
+        FidArray._is_iter_of_iters([[]])
+
+    def test_failed__is_iter_of_iters(self):
+        for i in [
+                [],
+                [1, 3],
+                [1, [2]],
+                ]:
+            self.assertFalse(FidArray._is_iter_of_iters(i))
 
     @staticmethod
     def _is_iter(i):
