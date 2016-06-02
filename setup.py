dfcb52fbbe20e4f6f9d136a4bf87f44ac092af20
Johann Eicher <johanneicher@gmail.com>
2016-06-02 13:13:09 +0200

    changed version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ config = {
     'author_email': 'johanneicher@gmail.com',
     'url': 'fake.address.com',
     #'download_url': 'Where to download it.',
-    'version': '1.0',
+    'version': '0.1',
     'install_requires': requirements,
     'packages': ['nmrpy', 'tests'],
     'license': 'New BSD',
