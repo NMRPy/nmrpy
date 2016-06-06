5bedd32c34eede8e24bc27b3b32098fad5b3f483
Johann Eicher <johanneicher@gmail.com>
2016-06-06 12:42:50 +0200

    cleaned

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:49,filenames:["index","intro"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to NMRPy&#8217;s documentation!","Introduction"],titleterms:{document:0,indice:0,introduct:1,nmrpy:0,tabl:0,welcom:0}})
