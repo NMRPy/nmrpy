8bcec61d96158d05024a39423ce0b4cb3e3722e7
Johann Eicher <johanneicher@gmail.com>
2016-06-07 17:24:23 +0200

    removed sphinx.ext.githubpages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+#    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
