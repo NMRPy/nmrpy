9e9c4b1f1771bec70a12254fcb67943cf348defc
JM Rohwer <j.m.rohwer@gmail.com>
2020-02-12 18:02:42 +0200

    new: update setup.py to copy tests and test files, as well as PDF docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -271,7 +271,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'NMRPy.tex', 'NMRPy Documentation',
-     'Johann Eicher', 'manual'),
+     'Johann Eicher, Johann Rohwer', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
