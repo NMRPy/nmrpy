d46f28f6bd97deb4118bf0e6cc110f7880fe6003
JM Rohwer <j.m.rohwer@gmail.com>
2020-02-13 21:40:57 +0200

    new: refactor tests so that they can be run from within the module

--- nmrpy/__init__.py
+++ nmrpy/__init__.py
@@ -1,2 +1,4 @@
 import nmrpy.data_objects
 from .version import __version__
+from nmrpy.tests import NMRPyTest as test
+
---
+++ nmrpy/tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+The test data used in this module consists of:
+
+    nmrpy/tests/test_data/bruker1    -- A Bruker spectrum of 2,3-diphospho-D-glyceric acid pentasodium salt 
+                                        from the Madison Metabolomics Consortium Database (expnmr_00002, 
+                                        http://mmcd.nmrfam.wisc.edu/rawnmr/expnmr_00001_1.tar)
+    nmrpy/tests/test_data/test1.fid  -- A Varian/Agilent array of the phosphoglucose-isomerase reaction
+    nmrpy/tests/test_data/test2.fid  -- A single Varian/Agilent spectrum of 3-phosphoglyceric acid, 
+                                        orthophosphate, and triethylphosphate
+"""
+
+from .nmrpy_tests import NMRPyTest
