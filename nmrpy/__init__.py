b7c21ef200e93589fdf5166c30e181d64b7170eb
JM Rohwer <j.m.rohwer@gmail.com>
2020-03-07 17:21:45 +0200

    chg: simplify instantiation of FidArray by calling nmrpy.from_path() directly

--- nmrpy/__init__.py
+++ nmrpy/__init__.py
@@ -2,3 +2,14 @@ import nmrpy.data_objects
 from .version import __version__
 from nmrpy.tests import NMRPyTest as test
 
+def from_path(fid_path='.', file_format=None):
+    """
+    Instantiate a new :class:`~nmrpy.data_objects.FidArray` object from a .fid directory.
+
+    :keyword fid_path: filepath to .fid directory
+
+    :keyword file_format: 'varian' or 'bruker', usually unnecessary
+
+    """
+    return nmrpy.data_objects.FidArray.from_path(fid_path, file_format)
+
